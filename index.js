f94463f6edff79085199c1433641793571c4acc2
ttmv <tmalmivi@cs.helsinki.fi>
2018-01-28 00:06:37 +0200

    t3.8

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const bodyParser = require('body-parser')
 const morgan = require('morgan')
 
 app.use(bodyParser.json())
-app.use(morgan('tiny'))
+
+morgan.token('body', function (req, res) { 
+    return JSON.stringify(req.body) 
+})
+
+app.use(morgan(':method :url :body :status :res[content-length] - :response-time ms'))
 
 let persons = [
     {
