8e921d8825df4f1cca8233f202ea8c11fcf30ad4
Titti M <tmalmivi@cs.helsinki.fi>
2018-01-29 15:57:04 +0200

    id:lla haku toimii

--- index.js
+++ index.js
@@ -74,11 +74,17 @@ app.get('/api/persons', (req, res) => {
 
 
 app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const person = persons.find(p => p.id === id)
-
-    if (person ) res.json(person)
-    else res.status(404).end()
+    Person.findById(req.params.id)
+        .then(person => {
+            if (person) {
+                res.json(Person.format(person))
+            } else {
+                res.status(404).end()
+            }
+        }).catch(err => {
+            console.log(err)
+            res.status(400).send({error: err})
+        })
 })
 
 app.put('/api/persons/:id', (req, res) => {
@@ -92,7 +98,7 @@ app.put('/api/persons/:id', (req, res) => {
         res.json(Person.format(updated))
     }).catch(err => {
         console.log(err)
-        response.status(400).send({ error: err })
+        res.status(400).send({ error: err })
     })
 })
 
